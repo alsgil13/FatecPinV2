adb84b12f863be702f47a663ec0fd542b56c0276
Lucas Caires <lucas@Mac-mini-de-Lucas.local>
2017-11-24 15:25:30 -0200

    Melhorando CORS

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,7 @@ const cors = require('./cors.js')
 const jwtMiddleware = require('./jwtMiddleware')
 
 //server.opts( /.*/, ( req, res ) => res.send( 204 ) )
-
+/*
 server.use(function(req, res, next) {
     if (req.method == 'OPTIONS') {
         res.send(200);
@@ -20,6 +20,38 @@ server.use(function(req, res, next) {
 server.pre(cors.preflight)
 
 server.use(cors.actual)
+*/
+
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(req.headers['x-access-token']) {
+        res.header('Access-Control-Allow-Headers', req.headers['x-access-token']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 
 server.use(restify.plugins.bodyParser())
 
