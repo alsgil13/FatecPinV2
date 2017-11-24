2eb66848d4ef5aee2ff74ed38659a3f2ec03ffb8
Lucas Caires <lucas@Mac-mini-de-Lucas.local>
2017-11-24 15:35:19 -0200

    Melhorando CORS

--- src/server/index.js
+++ src/server/index.js
@@ -24,28 +24,17 @@ server.use(cors.actual)
 
 server.use(function(req, res, next) {
     
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-	
-	// intercept OPTIONS method
-    if (req.method == 'OPTIONS') {
-        res.send(200);
-    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
     // Pass to next layer of middleware
     next();
 
 });
+
+
 
 server.use(restify.plugins.bodyParser())
 
