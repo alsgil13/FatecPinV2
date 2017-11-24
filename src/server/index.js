c5ba8ba596f5a8c8ab161a006a06718c578de967
Lucas Caires <lucas@Mac-mini-de-Lucas.local>
2017-11-24 15:36:24 -0200

    Melhorando CORS

--- src/server/index.js
+++ src/server/index.js
@@ -8,33 +8,9 @@ const cors = require('./cors.js')
 
 const jwtMiddleware = require('./jwtMiddleware')
 
-//server.opts( /.*/, ( req, res ) => res.send( 204 ) )
-/*
-server.use(function(req, res, next) {
-    if (req.method == 'OPTIONS') {
-        res.send(200);
-    }
-	next();
-});
-
 server.pre(cors.preflight)
 
 server.use(cors.actual)
-*/
-
-server.use(function(req, res, next) {
-    
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-
-    // Pass to next layer of middleware
-    next();
-
-});
-
-
 
 server.use(restify.plugins.bodyParser())
 
