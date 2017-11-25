4426d5465e70c8854604cd624a9ee26bae03360a
Lucas <lucas.cairesd@gmail.com>
2017-11-25 12:10:36 -0200

    ajuste servidor

--- src/server/index.js
+++ src/server/index.js
@@ -12,9 +12,7 @@ server.pre(cors.preflight) //cors
 
 server.use(cors.actual)
 
-server.use(restify.plugins.bodyParser({
-    mapParams: true
-}))
+server.use(restify.plugins.bodyParser())
 
 server.use(restify.plugins.queryParser())
 
