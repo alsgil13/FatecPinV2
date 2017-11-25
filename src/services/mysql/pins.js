b015304731c0fdef3c39dbfa616338eb6257127b
Lucas <lucas.cairesd@gmail.com>
2017-11-25 12:29:23 -0200

    ajuste servidor

--- src/services/mysql/pins.js
+++ src/services/mysql/pins.js
@@ -93,6 +93,7 @@ const pins = deps => {
 						errorHandler(error,'Falha ao salvar', reject)
 						return false
 					}
+					console.log(results)
 					resolve({pins: {tb_admins_idtb_admins, descricao, data_postagem, excluido, id: results.insertId}})
 				})
 				
